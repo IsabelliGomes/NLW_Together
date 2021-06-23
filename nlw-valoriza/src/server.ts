326474b7411642796c40523fb1b1462c14ffa60e
IsabelliGomes <IsabelliGomes@users.noreply.github.com>
2021-06-23 17:24:08 -0300

    NLW_Together - Aula 2
    Avanços com a Aula 2

--- nlw-valoriza/src/server.ts
+++ nlw-valoriza/src/server.ts
@@ -1,29 +1,26 @@
 
 import "reflect-metadata";
-import express, { request, response } from "express";
+import express from "express";
+import { router } from "./routes";
 
 import "./database";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(router);
+
+// http://localhost:3000
+app.listen(3000, () => console.log("Server is running"));
+
+
 /**
  * Tipos de parâmetros
- * 
+ *
  * Route Params => url + parâmetro específico
  * Query Params => url + ? + chave=valor + & + ...
  * Body Params => {
  *  arquivo json com valores
  * }
  */
-
-
-app.get("/test", (request, response) => {
-  return response.send("Hello!");
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Hello Post!");
-})
-
-// http://localhost:3000
-app.listen(3000, () => console.log("Server is running"));
