ceac597a0f2c997797530b2f777f85662a03d43d
IsabelliGomes <IsabelliGomes@users.noreply.github.com>
2021-06-26 12:36:56 -0300

    NLW_Together - Aula 4
    Aprendizados da aula 4

--- nlw-valoriza/src/routes.ts
+++ nlw-valoriza/src/routes.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { ensureAdmin } from "./middlewares/middleware";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/compliments", createComplimentController.handle);
+router.post("/login", authenticateUserController.handle);
 
 export { router };
---
+++ nlw-valoriza/src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+      email
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    const token = sign({
+      email: user.email
+    }, "1605b3c9c4edf4961b8784e490ed3239", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return token;
+  }
+
+}
+
+export { AuthenticateUserService };
